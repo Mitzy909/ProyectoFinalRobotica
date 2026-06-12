5337ebc65ed9b4880f9813a15cd8352e727e5762
Mitzy909 <mitzyaragon@comunidad.unam.mx>
2026-06-12 09:14:00 -0600

    Ajustes en urdf y rviz nombrados en estos codigos

--- ros/robot_ws/src/robot_bringup/setup.py
+++ ros/robot_ws/src/robot_bringup/setup.py
@@ -7,20 +7,27 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/rviz_bringup.launch.py',
-                                               'launch/rviz_bringup_2.launch.py',
-                                               'launch/rviz_bringup_3.launch.py',
-                                               'launch/trajectory_bringup.launch.py']),
-        
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            'share/' + package_name + '/launch',
+            [
+                'launch/rviz_bringup.launch.py',
+                'launch/trajectory_bringup.launch.py',
+            ]
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='robotics',
     maintainer_email='roberto.gar.1748@gmail.com',
-    description='TODO: Package description',
+    description='Paquete de lanzamiento para visualizar y mover el robot RRR en RViz.',
     license='TODO: License declaration',
     extras_require={
         'test': [
--- ros/robot_ws/src/robot_description/setup.py
+++ ros/robot_ws/src/robot_description/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 from glob import glob
+
 package_name = 'robot_description'
 
 setup(
@@ -7,20 +8,36 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + "/rviz", ["rviz/rviz.conf.rviz"]),
-        ('share/' + package_name + "/urdf", ["urdf/robot_rrr.urdf",
-                                             "urdf/robot_rrr_2.urdf",
-                                             "urdf/robot_rrr_3.urdf"]),
-        ('share/' + package_name + "/meshes", glob('meshes/*')) # Para incluir todos los archivos dentro de una carpeta con un solo comando
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+
+        (
+            'share/' + package_name + '/rviz',
+            ['rviz/rviz.conf.rviz']
+        ),
+
+        (
+            'share/' + package_name + '/urdf',
+            ['urdf/robot_rrr.urdf']
+        ),
+
+        (
+            'share/' + package_name + '/meshes',
+            glob('meshes/*')
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='robotics',
     maintainer_email='roberto.gar.1748@gmail.com',
-    description='TODO: Package description',
+    description='Modelo URDF y recursos visuales del robot RRR.',
     license='TODO: License declaration',
     extras_require={
         'test': [
